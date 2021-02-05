243cf4c9d19bcf67d6a98236a059d5a61ea51dd4
Ina Mulvik <inamulvik@outlook.com>
2021-02-05 13:53:07 +0100

    ok

--- main.js
+++ main.js
@@ -1,16 +1,3 @@
-/* function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
-  }
-
-window.onclick = function(e) {
-    if (!e.target.matches('.dropbtn')) {
-    var myDropdown = document.getElementById("myDropdown");
-      if (myDropdown.classList.contains('show')) {
-        myDropdown.classList.remove('show');
-      }
-    }
-  } */
-
 let overlayButtons = document.querySelectorAll('.overlay-trigger');
 
 overlayButtons.forEach(btn => {
